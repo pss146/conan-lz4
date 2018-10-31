7788360f877ee9746f9f54fc04751f21ee85d943
Michael Beach <michaelb@ieee.org>
2018-10-31 11:01:06 +1100

    Add gcc builds for armv7hf.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -12,10 +12,13 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        with tools.environment_append(RunEnvironment(self).vars):
-            if self.settings.os == "Windows":
-                self.run(os.path.join("bin","test_package"))
-            elif self.settings.os == "Macos":
-                self.run("DYLD_LIBRARY_PATH=%s %s"%(os.environ.get('DYLD_LIBRARY_PATH', ''),os.path.join("bin","test_package")))
-            else:
-                self.run("LD_LIBRARY_PATH=%s %s"%(os.environ.get('LD_LIBRARY_PATH', ''),os.path.join("bin","test_package")))
+        if tools.cross_building(self.settings):
+            self.output.warn("Skipping run cross built package")
+        else:
+            with tools.environment_append(RunEnvironment(self).vars):
+                if self.settings.os == "Windows":
+                    self.run(os.path.join("bin","test_package"))
+                elif self.settings.os == "Macos":
+                    self.run("DYLD_LIBRARY_PATH=%s %s"%(os.environ.get('DYLD_LIBRARY_PATH', ''),os.path.join("bin","test_package")))
+                else:
+                    self.run("LD_LIBRARY_PATH=%s %s"%(os.environ.get('LD_LIBRARY_PATH', ''),os.path.join("bin","test_package")))
