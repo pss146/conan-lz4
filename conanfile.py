bb2d07ddb33e2ae6e5a0991e4b548df83d21c70f
SSE4 <tomskside@gmail.com>
2019-02-15 04:17:01 +0700

    - disable code analysis
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -63,6 +63,11 @@ class LZ4Conan(ConanFile):
     def build_vs(self):
         shutil.copy(os.path.join(self.source_subfolder, "lib", "lz4.h"),
                     os.path.join(self.source_subfolder, "visual", "VS2010", "liblz4-dll", "lz4.h"))
+        # Unable to load plug-in localespc.dll
+        tools.replace_in_file(os.path.join(self.source_subfolder, "visual", "VS2010", "liblz4", "liblz4.vcxproj"),
+                              "<RunCodeAnalysis>true</RunCodeAnalysis>", "")
+        tools.replace_in_file(os.path.join(self.source_subfolder, "visual", "VS2010", "liblz4-dll", "liblz4-dll.vcxproj"),
+                              "<RunCodeAnalysis>true</RunCodeAnalysis>", "")
         with tools.chdir(os.path.join(self.source_subfolder, 'visual', 'VS2010')):
             target = 'liblz4-dll' if self.options.shared else 'liblz4'
 
